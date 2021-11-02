91f7e37e06cacc0074416199791120b62e88828c
rithiksinha <rithiksinha2000@gmail.com>
2021-11-02 16:10:31 +0530

    Initial Commit

---
+++ testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Ritik Sinha on 02/11/21.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
